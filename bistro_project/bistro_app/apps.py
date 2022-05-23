806b3ec4cbf2437405842140ff891c511528ba90
Titov97 <tyty_fylls@yahoo.com>
2022-05-23 23:43:38 +0300

    bistro_app , first commit

---
+++ bistro_project/bistro_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BitroAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bistro_app'
---
+++ bistro_project/bistro_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Ingredients(models.Model):
+    ingredients = models.CharField(max_length=128)
+    quantity_stock = models.FloatField()
+    unit = models.CharField(max_length=128)
+    price_unit = models.FloatField()
+    min_stock = models.FloatField()
+    alert_stock = models.FloatField()
+    remarks = models.CharField(max_length=128)
+    lot = models.CharField(max_length=128)
+    entry_date = models.DateTimeField(auto_now_add=True)
+    expiry_date = models.DateTimeField(null=False)
+
+
+class Recipe(models.Model):
+    pass
+
+
+class Menu(models.Model):
+    pass
+
+
+class Order(models.Model):
+    pass
