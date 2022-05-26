d5cb6aeec80b8519214ee561d638bd1b86e580bd
Radu Iordache <radu_iordache_83@yahoo.com>
2022-05-26 22:43:44 +0300

    Admin in

--- Bistro_P/bistro_app/admin.py
+++ Bistro_P/bistro_app/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 #TODO RADU - admin
 
 # Register your models here.
+from Bistro_P.bistro_app.models import *
+
+admin.site.register(Ingredients)
+admin.site.register(Recipe)
+admin.site.register(Menu)
+admin.site.register(Order)
