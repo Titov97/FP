e8c563c372cc2e94c0d4fefd578a207fef81fe54
Radu Iordache <radu_iordache_83@yahoo.com>
2022-06-28 20:19:41 +0300

    todo added

--- Bistro_P/bistro_app/models.py
+++ Bistro_P/bistro_app/models.py
@@ -31,17 +31,40 @@ class Recipe(models.Model):
     # created_by = models.CharField(max_length=128)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     ingredients = models.ManyToManyField(RecipeIngredient)
+    sale_price = models.FloatField()
+
+    def production_price(self):
+        # total_cost = 0
+        # for recipe_ingredient in self.ingredients.all():
+        #     total_cost += recipe_ingredient.ingredient.price_unit * recipe_ingredient.quantity
+        # return total_cost
+        return (recipe_ingredient.ingredient.price_unit * recipe_ingredient.quantity for recipe_ingredient in
+                self.ingredients.all())
 
 
 class Menu(models.Model):
-    #manytomany recipe
-    pass
+    menu_name = models.CharField(max_length=255)
+    sale_price = models.FloatField()
+
+
+
+class MenuItem(models.Model):
+    recipe = models.ForeignKey(RecipeIngredient, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.recipe} {self.quantity}"
+
 
 class Order(models.Model):
-    pass
+    #status = models.Choices #TODO Radu
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 
 class OrderItem(models.Model):
-    pass
-
+    recipe = models.ForeignKey(RecipeIngredient, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
 
---
+++ Bistro_P/todo.md
@@ -0,0 +1,22 @@
+Alex:
+
+- homepage
+- menu page
+- navbar
+
+Radu:
+
+- contact page - / feedback
+- recipe_create_page ?? clarificari
+
+Sebi:
+
+- checkout
+- add to cart
+- cart/ open order
+
+Filip:
+
+- login
+- register
+- recover_pass flow (4 templates)
