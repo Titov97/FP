917cc58cc59a770fc8118d81445573bab3aef563
Radu Iordache <radu_iordache_83@yahoo.com>
2022-07-18 15:10:50 +0300

    recipes table added - v1

--- Bistro_P/bistro_app/urls.py
+++ Bistro_P/bistro_app/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('search/', search_view, name="search_results"),
     path('cart/', views.open_cart_view, name="open_cart"),
+    path('special/', views.open_cart_view, name="special"),
 ]
