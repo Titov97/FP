b2eb5828c4ee2b7fa0e826f8c8851612cc5f1b4c
Titov97 <tyty_fylls@yahoo.com>
2022-07-18 20:06:14 +0300

    fixed menu page

--- Bistro_P/bistro_app/forms.py
+++ Bistro_P/bistro_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 import csv
-from bistro_app.models import Ingredient
+from bistro_app.models import Ingredient, Menu
 
 
 class IngredientsForm(forms.ModelForm):
@@ -91,4 +91,6 @@ class SignUpForm(UserCreationForm):
             # 'password1': forms.PasswordInput(attrs={'placeholder': 'Password'}),
             # 'password2': forms.PasswordInput(attrs={'placeholder': 'Re-entry your password'}),
         }
+
+
 
