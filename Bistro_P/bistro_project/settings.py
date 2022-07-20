f2553d3bf513217c4428bacc694ffad4238842ce
Radu Iordache <radu_iordache_83@yahoo.com>
2022-07-20 20:00:09 +0300

    add allowed host

--- Bistro_P/bistro_project/settings.py
+++ Bistro_P/bistro_project/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'index'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
